f745b3fe511c01a93ce0b1f7a86e51da60fa3ced
priyakamble2004 <pr26iya12@gmail.com>
2026-03-07 15:28:44 -0800

    add deployment files

--- config.py
+++ config.py
@@ -3,11 +3,11 @@ from datetime import timedelta
 from urllib.parse import quote_plus
 
 class Config:
-    MYSQL_HOST     = os.getenv('MYSQL_HOST',     'localhost')
-    MYSQL_PORT     = int(os.getenv('MYSQL_PORT', 3306))
-    MYSQL_USER     = os.getenv('MYSQL_USER',     'root')
-    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', 'priya@2004')
-    MYSQL_DATABASE = os.getenv('MYSQL_DATABASE', 'brainboost_v2')
+    MYSQL_HOST     = os.getenv('MYSQLHOST',     'tramway.proxy.rlwy.net')
+    MYSQL_PORT     = int(os.getenv('MYSQLPORT', 53271))
+    MYSQL_USER     = os.getenv('MYSQLUSER',     'root')
+    MYSQL_PASSWORD = os.getenv('MYSQLPASSWORD', 'wTNPRIMnjMCDjIrMOqwauYokOAvQryth')
+    MYSQL_DATABASE = os.getenv('MYSQLDATABASE', 'railway')
 
     SQLALCHEMY_DATABASE_URI = (
         f"mysql+pymysql://{MYSQL_USER}:{quote_plus(MYSQL_PASSWORD)}"
@@ -22,3 +22,8 @@ class Config:
     UPLOAD_FOLDER       = os.path.join(os.path.dirname(__file__), 'uploads')
     MAX_CONTENT_LENGTH  = 5 * 1024 * 1024
     ALLOWED_EXTENSIONS  = {'png', 'jpg', 'jpeg', 'gif', 'webp'}
+
+
+
+
+
